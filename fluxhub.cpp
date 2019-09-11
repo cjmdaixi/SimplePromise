9de2461697df5d63322faf74abbe971a46d65eb6
Jinming Chen <chenjinming@shining3d.com>
2019-09-11 20:27:13 +0800

    use QtConCurrent::run instead

--- fluxhub.cpp
+++ fluxhub.cpp
@@ -1,7 +1,7 @@
 #include "fluxhub.h"
 #include <QRunnable>
 #include <QThreadPool>
-#include <QFutureInterface>
+#include <QtConcurrent>
 
 FluxHub::FluxHub(QObject *parent) : QObject(parent)
 {
@@ -15,169 +15,72 @@ SimplePromise * FluxHub::promise()
 
 QFuture<Result> FluxHub::asyncFuncA(QString msg)
 {
-    class Runnable: public QRunnable
-    {
-    public:
-        Runnable(QString msg, QFutureInterface<Result> futureInterface)
-            : m_msg(msg), m_futureInterface(futureInterface)
-        {}
-        virtual void run() override{
-            m_futureInterface.reportStarted();
+    auto future = QtConcurrent::run([](QString msg)->Result{
             qDebug()<<"async func A running...";
             QThread::currentThread()->msleep(3000);
-            Result result(QVariant::fromValue(m_msg + " funcA"));
-            m_futureInterface.reportResult(&result);
+            Result result(QVariant::fromValue(msg + " funcA"));
             qDebug()<<"async func A stopped...";
-            m_futureInterface.reportFinished();
-        }
-        QString m_msg;
-        QFutureInterface<Result> m_futureInterface;
-
-    };
-
-    QFutureInterface<Result> futureInterface;
-    QThreadPool::globalInstance()->start(new Runnable(msg, futureInterface));
-    auto future = futureInterface.future();
+            return result;
+}, msg);
     return future;
 }
 
 QFuture<Result> FluxHub::asyncFuncB(QString msg)
 {
-    class Runnable: public QRunnable
-    {
-    public:
-        Runnable(QString msg, QFutureInterface<Result> futureInterface)
-            : m_msg(msg), m_futureInterface(futureInterface)
-        {}
-        virtual void run() override{
-            m_futureInterface.reportStarted();
+    auto future = QtConcurrent::run([](QString msg)->Result{
             qDebug()<<"async func B running...";
             QThread::currentThread()->msleep(3000);
-            Result result(QVariant::fromValue(m_msg + " funcB"));
-            m_futureInterface.reportResult(&result);
+            Result result(QVariant::fromValue(msg + " funcB"));
             qDebug()<<"async func B stopped...";
-            m_futureInterface.reportFinished();
-        }
-        QString m_msg;
-        QFutureInterface<Result> m_futureInterface;
-
-    };
-
-    QFutureInterface<Result> futureInterface;
-    QThreadPool::globalInstance()->start(new Runnable(msg, futureInterface));
-    auto future = futureInterface.future();
+            return result;
+}, msg);
     return future;
 }
 
 QFuture<Result> FluxHub::asyncFuncC(QString msg)
 {
-    class Runnable: public QRunnable
-    {
-    public:
-        Runnable(QString msg, QFutureInterface<Result> futureInterface)
-            : m_msg(msg), m_futureInterface(futureInterface)
-        {}
-        virtual void run() override{
-            m_futureInterface.reportStarted();
+    auto future = QtConcurrent::run([](QString msg)->Result{
             qDebug()<<"async func C running...";
             QThread::currentThread()->msleep(3000);
-            Result result(QVariant::fromValue(m_msg + " funcC"));
-            m_futureInterface.reportResult(&result);
+            Result result(QVariant::fromValue(msg + " funcC"));
             qDebug()<<"async func C stopped...";
-            m_futureInterface.reportFinished();
-        }
-        QString m_msg;
-        QFutureInterface<Result> m_futureInterface;
-
-    };
-
-    QFutureInterface<Result> futureInterface;
-    QThreadPool::globalInstance()->start(new Runnable(msg, futureInterface));
-    auto future = futureInterface.future();
+            return result;
+}, msg);
     return future;
 }
 
 QFuture<Result> FluxHub::asyncFuncD(int val)
 {
-    class Runnable: public QRunnable
-    {
-    public:
-        Runnable(int val, QFutureInterface<Result> futureInterface)
-            : m_val(val), m_futureInterface(futureInterface)
-        {}
-        virtual void run() override{
-            m_futureInterface.reportStarted();
-            qDebug()<<"async func D running...";
-            QThread::currentThread()->msleep(3000);
-            Result result(QVariant::fromValue(m_val + 1));
-            m_futureInterface.reportResult(&result);
-            qDebug()<<"async func D stopped...";
-            m_futureInterface.reportFinished();
-        }
-        int m_val;
-        QFutureInterface<Result> m_futureInterface;
-
-    };
-
-    QFutureInterface<Result> futureInterface;
-    QThreadPool::globalInstance()->start(new Runnable(val, futureInterface));
-    auto future = futureInterface.future();
+    auto future = QtConcurrent::run([](int val)->Result{
+        qDebug()<<"async func D running...";
+        QThread::currentThread()->msleep(3000);
+        Result result(QVariant::fromValue(val + 1));
+        qDebug()<<"async func D stopped...";
+        return result;
+    }, val);
     return future;
 }
 
 QFuture<Result> FluxHub::asyncFuncE(QString msg, int val)
 {
-    class Runnable: public QRunnable
-    {
-    public:
-        Runnable(QString msg, int val, QFutureInterface<Result> futureInterface)
-            : m_msg(msg), m_val(val), m_futureInterface(futureInterface)
-        {}
-        virtual void run() override{
-            m_futureInterface.reportStarted();
+    auto future = QtConcurrent::run([](QString msg, int val)->Result{
             qDebug()<<"async func E running...";
             QThread::currentThread()->msleep(3000);
-            Result result(QVariant::fromValue(m_msg + " funcE" + QString(" %1").arg(m_val)));
-            m_futureInterface.reportResult(&result);
+            Result result(QVariant::fromValue(msg + " funcE" + QString(" %1").arg(val)));
             qDebug()<<"async func E stopped...";
-            m_futureInterface.reportFinished();
-        }
-        QString m_msg;
-        int m_val;
-        QFutureInterface<Result> m_futureInterface;
-
-    };
-
-    QFutureInterface<Result> futureInterface;
-    QThreadPool::globalInstance()->start(new Runnable(msg, val, futureInterface));
-    auto future = futureInterface.future();
+            return result;
+}, msg, val);
     return future;
 }
 
 QFuture<Result> FluxHub::asyncFuncF(QString msg)
 {
-    class Runnable: public QRunnable
-    {
-    public:
-        Runnable(QString msg, QFutureInterface<Result> futureInterface)
-            : m_msg(msg), m_futureInterface(futureInterface)
-        {}
-        virtual void run() override{
-            m_futureInterface.reportStarted();
+    auto future = QtConcurrent::run([](QString msg)->Result{
             qDebug()<<"async func F running...";
             QThread::currentThread()->msleep(3000);
-            Result result(QVariant::fromValue(m_msg + " funcF"));
-            m_futureInterface.reportResult(&result);
+            Result result(QVariant::fromValue(msg + " funcF"));
             qDebug()<<"async func F stopped...";
-            m_futureInterface.reportFinished();
-        }
-        QString m_msg;
-        QFutureInterface<Result> m_futureInterface;
-
-    };
-
-    QFutureInterface<Result> futureInterface;
-    QThreadPool::globalInstance()->start(new Runnable(msg, futureInterface));
-    auto future = futureInterface.future();
+            return result;
+}, msg);
     return future;
 }
